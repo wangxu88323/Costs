22aa6acf00988ee07cf9fb51dcf240467860a145
WANG Xu <redbank3@qq.com>
2015-04-15 09:48:59 +0800

    test

--- Application/Test/Controller/TestShowPrintersController.class.php
+++ Application/Test/Controller/TestShowPrintersController.class.php
@@ -9,6 +9,16 @@ namespace Test\Controller;
 use Think\Controller;
 class TestShowPrintersController extends Controller{
     public function index(){
+        $Printers = D('Printers');
+        $Depts = $Printers->field('dept')->select();
+        $depts = array();
+        for( $i = 0 ; $i < count($Depts) ; $i ++ ){
+            $depts[] = $Depts[$i]['dept'];
+        }
+        $depts = array_unique($depts);
+        sort($depts);
+        dump($depts);
+        //dump($Depts);
         $this->display();
     }
 }
