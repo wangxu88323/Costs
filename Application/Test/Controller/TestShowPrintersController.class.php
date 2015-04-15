dbac8513037a76ac93b798108a3691815ebd59d1
WANG Xu <redbank3@qq.com>
2015-04-15 16:24:21 +0800

    统计各部门打印机数量，测试成功，可以合并到master

--- Application/Test/Controller/TestShowPrintersController.class.php
+++ Application/Test/Controller/TestShowPrintersController.class.php
@@ -16,9 +16,49 @@ class TestShowPrintersController extends Controller{
             $depts[] = $Depts[$i]['dept'];
         }
         $depts = array_unique($depts);
-        sort($depts);
-        dump($depts);
+        rsort($depts);
+        //dump($depts);
         //dump($Depts);
+        for( $i = 0 ; $i < count($depts) ; $i ++ ){
+            $depts[$i] = "'" . $depts[$i] . "'";
+        }
+        $yAxis = implode(',',$depts);
+        //$yAxis = "'" . "打印机总数" . "'," . $yAxis;
+        $param['yAxis'] = $yAxis;
+
+        /*$data = array();
+        for( $i = 0 ; $i < count($depts) ; $i ++ ){
+            $data[] = rand(50,60);
+        }
+        $xAxis = implode(',',$data);
+        $param['xAxis'] = $xAxis;*/
+        //TODO
+        $data = array();
+        //$total = 0;
+        for( $i = 0 ; $i < count($depts) ; $i ++ ){
+            $count = $Printers->where("dept=" . $depts[$i])->count();
+            //$total += $count;
+            $data[] = $count;
+        }
+        $xAxis = implode(',',$data);
+        //$xAxis = $total . ',' . $xAxis;
+        $param['xAxis'] = $xAxis;
+
+
+        $this->assign($param);
         $this->display();
+    }
+
+    public function getDeptPrinters(){
+        $dept = I('post.dept');
+        $data['dept'] = $dept;
+        $Printers = D('Printers');
+        $deptPrinters = $Printers->field('sn,model,supplies')->where("dept='" . $dept . "'")->select();
+        /*for( $i = 0 ; $i < 10 ; $i ++ ){
+            $data['printers'][] = rand(20,30);
+        }*/
+        $data['sql'] = $Printers->getLastSql();
+        $data['printers'] = $deptPrinters;
+        $this->ajaxReturn($data);
     }
 }
